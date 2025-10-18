cceaa83323bb54ee475a80e6d92b82343956ccee
Mau007 <lic.mauricio_flores@hotmail.com>
2025-10-17 18:03:21 -0600

    cambiosM

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nuevo App',
+      title: 'CAmbios en la APP',
       theme: ThemeData(
         // This is the theme of your application.
         //
